39a93d383bc2437ebd65d7c0dd7a42a46e308de4
DevMiguell <buguel2000@gmail.com>
2021-05-11 14:37:10 -0300

    configurando fake-api com axios

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs';
 import { App } from './App';
+
+createServer({
+  routes() {
+    this.namespace = 'api'; // pegar todas as chamada com "api" e direcionar para o mirage js
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 400,
+          type: "deposit",
+          category: "food",
+          createdAt: new Date()
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
