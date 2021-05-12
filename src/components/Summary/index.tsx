36614e20c871b82bf3f92afe4e1e83625e9af047
DevMiguell <buguel2000@gmail.com>
2021-05-12 15:24:12 -0300

    Criando o Contexto da aplicação

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,30 +1,34 @@
-import incomeImg from '../../assets/income.svg'
-import outcomeImg from '../../assets/outcome.svg'
-import totalImg from '../../assets/total.svg'
+import { useContext } from 'react';
+import { TransactionsContext } from '../../TransactionsContext';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import totalImg from '../../assets/total.svg';
 
 import { Container } from "./styles";
 
 export function Summary() {
-  return(
+  const { transactions } = useContext(TransactionsContext);
+
+  return (
     <Container>
       <div>
         <header>
           <p>Entradas</p>
-          <img src={incomeImg} alt="Entradas"/>
+          <img src={incomeImg} alt="Entradas" />
         </header>
         <strong>R$1000,00</strong>
       </div>
       <div>
         <header>
           <p>Saidas</p>
-          <img src={outcomeImg} alt="Saidas"/>
+          <img src={outcomeImg} alt="Saidas" />
         </header>
         <strong>- R$500,00</strong>
       </div>
       <div className="highlight-background">
         <header>
           <p>Total</p>
-          <img src={totalImg} alt="Total"/>
+          <img src={totalImg} alt="Total" />
         </header>
         <strong>R$500,00</strong>
       </div>
